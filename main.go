90ebdb607ebe50ac3c2319007de62e5ded423d53
ottojo <jottosmail@gmail.com>
2017-04-23 23:06:20 +0200

    unExported some types, typos

--- main.go
+++ main.go
@@ -11,37 +11,37 @@ import (
 )
 
 var resistorListFile string
-var Uin float64
-var Uout float64
+var uIn float64
+var uOut float64
 var numResults int
 
-type Resistor struct {
+type resistor struct {
 	packageType string
 	resistance  int
 }
 
-type UDiv struct {
-	r1, r2 Resistor
+type uDiv struct {
+	r1, r2 resistor
 }
 
-func (uDiv UDiv) voltageDivision(uin float64) (uout float64) {
-	return uin * (float64(uDiv.r2.resistance) / float64(uDiv.r1.resistance+uDiv.r2.resistance))
+func (u uDiv) voltageDivision(uin float64) (uout float64) {
+	return uin * (float64(u.r2.resistance) / float64(u.r1.resistance+u.r2.resistance))
 }
 
-type ByDeviation []UDiv
+type byDeviation []uDiv
 
-func (uDiv ByDeviation) Len() int      { return len(uDiv) }
-func (uDiv ByDeviation) Swap(i, j int) { uDiv[i], uDiv[j] = uDiv[j], uDiv[i] }
-func (uDiv ByDeviation) Less(i, j int) bool {
-	return math.Abs(uDiv[i].voltageDivision(Uin)-Uout) < math.Abs(uDiv[j].voltageDivision(Uin)-Uout)
+func (u byDeviation) Len() int      { return len(u) }
+func (u byDeviation) Swap(i, j int) { u[i], u[j] = u[j], u[i] }
+func (u byDeviation) Less(i, j int) bool {
+	return math.Abs(u[i].voltageDivision(uIn)-uOut) < math.Abs(u[j].voltageDivision(uIn)-uOut)
 }
 
 func init() {
-	flag.StringVar(&resistorListFile, "resistors", "", "File containing availiable resistors:"+
+	flag.StringVar(&resistorListFile, "resistors", "", "File containing available resistors:"+
 		" Each line should contain the package description and,"+
-		" seperated by at least one space, the resistance in ohms.")
-	flag.Float64Var(&Uin, "Uin", 1.0, "Input voltage in volts")
-	flag.Float64Var(&Uout, "Uout", 0.5, "Output voltag in volts")
+		" separated by at least one space, the resistance in ohms.")
+	flag.Float64Var(&uIn, "Uin", 1.0, "Input voltage in volts")
+	flag.Float64Var(&uOut, "Uout", 0.5, "Output voltag in volts")
 	flag.IntVar(&numResults, "n", -1, "Number of dividers to calculate")
 	flag.Parse()
 }
@@ -54,14 +54,14 @@ func main() {
 	resistorsString := strings.Replace(string(resistors), "\r", "", -1)
 	resistorValues := parseResistorList(resistorsString)
 
-	var dividers []UDiv
+	var dividers []uDiv
 	for _, r1 := range resistorValues {
 		for _, r2 := range resistorValues {
-			dividers = append(dividers, UDiv{r1, r2})
+			dividers = append(dividers, uDiv{r1, r2})
 		}
 	}
 
-	sort.Sort(ByDeviation(dividers))
+	sort.Sort(byDeviation(dividers))
 
 	if numResults == -1 || numResults > len(dividers) {
 		numResults = len(dividers)
@@ -69,11 +69,11 @@ func main() {
 	for i := 0; i < numResults; i++ {
 		log.Print("Divider " + strconv.Itoa(i) + ": ")
 		log.Println(dividers[i])
-		log.Println(dividers[i].voltageDivision(Uin))
+		log.Println(dividers[i].voltageDivision(uIn))
 	}
 }
 
-func parseResistorList(list string) (resistorValues []Resistor) {
+func parseResistorList(list string) (resistorValues []resistor) {
 	resistorList := strings.Split(list, "\n")
 	for i := 0; i < len(resistorList); i++ {
 		if strings.HasPrefix(resistorList[i], "#") || strings.HasPrefix(resistorList[i], "//") || len(resistorList[i]) == 0 {
@@ -92,7 +92,7 @@ func parseResistorList(list string) (resistorValues []Resistor) {
 		if err != nil {
 			continue
 		}
-		resistorValues = append(resistorValues, Resistor{packageString, value})
+		resistorValues = append(resistorValues, resistor{packageString, value})
 	}
 	return resistorValues
 }
